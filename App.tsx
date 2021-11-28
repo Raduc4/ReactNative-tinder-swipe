0a870c9117bdf487cdf4c356b0a73370745115a8
Radu Cazacu <cazacuraduic@gmail.com>
2021-11-28 14:42:10 +0200

    Init commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,222 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Dimensions,
+} from "react-native";
+import tw from "twrnc";
+
+import data from "./data";
+import Swiper from "react-native-deck-swiper";
+import { Transitioning, Transition } from "react-native-reanimated";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const { width } = Dimensions.get("window");
+
+const ANIMATION_DURATION = 200;
+
+const transition = (
+  <Transition.Sequence>
+    <Transition.Out
+      type="slide-bottom"
+      durationMs={ANIMATION_DURATION}
+      interpolation="easeIn"
+    />
+    <Transition.Together>
+      <Transition.In
+        type="fade"
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+      />
+      <Transition.In
+        type="slide-bottom"
+        durationMs={ANIMATION_DURATION}
+        delayMs={ANIMATION_DURATION / 2}
+        interpolation="easeOut"
+      />
+    </Transition.Together>
+  </Transition.Sequence>
+);
+
+const stackSize = 4;
+const colors = {
+  red: "#EC2379",
+  blue: "#0070FF",
+  gray: "#777777",
+  white: "#ffffff",
+  black: "#000000",
+};
+
+const Card = ({ card }: any) => {
+  return (
+    <View style={styles.card}>
+      <Image
+        source={{ uri: card.image }}
+        style={tw.style("flex-1", styles.cardImage)}
+      />
+    </View>
+  );
+};
+
+const CardDetails = ({ index }: any) => (
+  <View key={data[index].id} style={{ alignItems: "center" }}>
+    <Text style={[styles.text, styles.heading]} numberOfLines={2}>
+      {data[index].name}
+    </Text>
+    <Text style={[styles.text, styles.price]}>{data[index].price}</Text>
+  </View>
+);
 
 export default function App() {
+  const swiperRef = React.useRef<any>();
+  const transitionRef = React.useRef<any>();
+
+  const [index, setIndex] = React.useState(0);
+  const onSwiped = () => {
+    transitionRef.current.animateNextTransition();
+    setIndex((index + 1) % data.length);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tw`flex-1 bg-white`}>
+      <MaterialCommunityIcons
+        name="crop-square"
+        size={width}
+        color={colors.blue}
+        style={{
+          opacity: 0.05,
+          transform: [{ rotate: "45deg" }, { scale: 1.6 }],
+          position: "absolute",
+          left: -15,
+          top: 30,
+        }}
+      />
+      <StatusBar hidden={true} />
+
+      <View style={{ flex: 0.55 }}>
+        <Swiper
+          ref={swiperRef}
+          cards={data}
+          renderCard={(card) => <Card card={card} />}
+          cardIndex={index}
+          infinite
+          backgroundColor={"transparent"}
+          onSwiped={onSwiped}
+          onTapCard={() => swiperRef.current.swipeLeft()}
+          cardVerticalMargin={50}
+          stackSize={stackSize}
+          stackScale={10}
+          stackSeparation={14}
+          animateOverlayLabelsOpacity
+          animateCardOpacity
+          disableTopSwipe
+          disableBottomSwipe
+          overlayLabels={{
+            left: {
+              title: "NOPE",
+              style: {
+                label: {
+                  backgroundColor: colors.red,
+                  borderColor: colors.red,
+                  color: colors.white,
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-end",
+                  justifyContent: "flex-start",
+                  marginTop: 20,
+                  marginLeft: -20,
+                },
+              },
+            },
+            right: {
+              title: "LIKE",
+              style: {
+                label: {
+                  backgroundColor: colors.blue,
+                  borderColor: colors.blue,
+                  color: colors.white,
+                  borderWidth: 1,
+                  fontSize: 24,
+                },
+                wrapper: {
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                  marginTop: 20,
+                  marginLeft: 20,
+                },
+              },
+            },
+          }}
+        />
+      </View>
+
+      <View style={tw.style("justify-evenly", { flex: 0.45 })}>
+        <Transitioning.View
+          ref={transitionRef}
+          transition={transition}
+          style={tw.style("justify-end items-center")}
+        >
+          <CardDetails index={index} />
+        </Transitioning.View>
+        <View style={tw.style("flex-row justify-evenly")}>
+          <MaterialCommunityIcons.Button
+            name="close"
+            size={94}
+            backgroundColor="transparent"
+            underlayColor="transparent"
+            activeOpacity={0.3}
+            color={colors.red}
+            onPress={() => swiperRef.current.swipeLeft()}
+          />
+          <MaterialCommunityIcons.Button
+            name="circle-outline"
+            size={94}
+            backgroundColor="transparent"
+            underlayColor="transparent"
+            activeOpacity={0.3}
+            color={colors.blue}
+            onPress={() => swiperRef.current.swipeRight()}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  cardImage: {
+    width: 160,
+    resizeMode: "contain",
   },
+  card: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: colors.black,
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.white,
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent",
+  },
+  done: {
+    textAlign: "center",
+    fontSize: 30,
+    color: colors.white,
+    backgroundColor: "transparent",
+  },
+  heading: { fontSize: 24, marginBottom: 10, color: colors.gray },
+  price: { color: colors.blue, fontSize: 32, fontWeight: "500" },
 });
